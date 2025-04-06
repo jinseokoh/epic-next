8157f0a138cb3bce9c9dce613a7483ab2c7db77b
Chuck JS. Oh <jinseokoh@hotmail.com>
2025-04-06 19:50:03 +0900

    epic blog part #5

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,5 @@
+import { SigninForm } from "@/app/(auth)/signin/signin-form";
+
+export default function SignInRoute() {
+  return <SigninForm />;
+}
---
+++ src/app/(auth)/signin/signin-form.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { loginUserAction } from "@/data/actions/auth-actions";
+import Link from "next/link";
+import { useActionState } from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { StrapiErrors } from "@/components/custom/strapi-errors";
+import { SubmitButton } from "@/components/custom/submit-button";
+import { ZodErrors } from "@/components/custom/zod-errors";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const INITIAL_STATE = {
+  zodErrors: null,
+  strapiErrors: null,
+  data: null,
+  message: null,
+};
+
+export function SigninForm() {
+  const [formState, formAction] = useActionState(loginUserAction, INITIAL_STATE);
+  return (
+    <div className="w-full max-w-md">
+      <form action={formAction}>
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-3xl font-bold">Sign In</CardTitle>
+            <CardDescription>
+              Enter your details to sign in to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="identifier"
+                name="identifier"
+                type="text"
+                placeholder="username or email"
+              />
+              <ZodErrors error={formState?.zodErrors?.identifier} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="password"
+              />
+              <ZodErrors error={formState?.zodErrors?.password} />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <SubmitButton
+              className="w-full"
+              text="Sign In"
+              loadingText="Loading"
+            />
+            <StrapiErrors error={formState?.strapiErrors} />
+          </CardFooter>
+        </Card>
+        <div className="mt-4 text-center text-sm">
+          Don't have an account?
+          <Link className="underline ml-2" href="signup">
+            Sign Up
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
